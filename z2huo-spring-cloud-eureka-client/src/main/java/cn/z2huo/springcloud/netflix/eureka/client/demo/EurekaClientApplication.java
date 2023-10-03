f6d07024882e652df0a8dbbfcff0390762128a97
z2huo <z2huo9994@163.com>
2023-10-03 23:31:18 +0800

    spring cloud eureka region and zoneˆ

---
+++ z2huo-spring-cloud-eureka-client/src/main/java/cn/z2huo/springcloud/netflix/eureka/client/demo/EurekaClientApplication.java
@@ -0,0 +1,33 @@
+package cn.z2huo.springcloud.netflix.eureka.client.provider;
+
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
+import org.springframework.context.ConfigurableApplicationContext;
+import org.springframework.core.env.Environment;
+
+import java.net.InetAddress;
+import java.net.UnknownHostException;
+import java.util.Arrays;
+
+@Slf4j
+@EnableDiscoveryClient
+@SpringBootApplication
+public class EurekaClientApplication {
+
+    public static void main(String[] args) {
+        ConfigurableApplicationContext context = SpringApplication.run(EurekaClientApplication.class, args);
+        Environment environment = context.getEnvironment();
+        log.info("active profiles contains: {}", Arrays.toString(environment.getActiveProfiles()));
+
+        InetAddress local = null;
+        try {
+            local = InetAddress.getLocalHost();
+            log.info("server start, URL is http://{}:{}", local.getHostAddress(), environment.getProperty("server.port"));
+        } catch (UnknownHostException e) {
+            log.error("InetAddress get failed", e);
+        }
+    }
+
+}
---
+++ z2huo-spring-cloud-eureka-server/src/main/java/cn/z2huo/springcloud/netflix/eureka/server/EurekaServerApplication.java
@@ -0,0 +1,68 @@
+package cn.z2huo.springcloud.netflix.eureka.server;
+
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.eureka.server.EnableEurekaServer;
+import org.springframework.context.ConfigurableApplicationContext;
+import org.springframework.core.env.Environment;
+
+import java.net.InetAddress;
+import java.net.UnknownHostException;
+import java.util.Arrays;
+
+@Slf4j
+@EnableEurekaServer
+@SpringBootApplication
+public class EurekaServerApplication {
+
+    public static void main(String[] args) throws UnknownHostException {
+        // spring cloud官网文档里面是这样启动的
+//        new SpringApplicationBuilder(Application.class).web(true).run(args);
+
+        ConfigurableApplicationContext context = SpringApplication.run(EurekaServerApplication.class, args);
+        Environment environment = context.getEnvironment();
+        log.info("active profiles contains: {}", Arrays.toString(environment.getActiveProfiles()));
+
+        InetAddress local = null;
+        try {
+            local = InetAddress.getLocalHost();
+//            System.out.println(local.getHostAddress());
+//            System.out.println(local.getHostName());
+//            System.out.println(Arrays.toString(local.getAddress()));
+//            System.out.println(local.getCanonicalHostName());
+            log.info("server start, URL is http://{}:{}", local.getHostAddress(), environment.getProperty("server.port"));
+        } catch (UnknownHostException e) {
+            log.error("InetAddress get failed", e);
+        }
+
+//        System.out.println(">>> environment");
+//        System.out.println(environment.getProperty("class"));
+//        System.out.println("> system properties");
+//        System.out.println(environment.getProperty("name"));
+//        System.out.println(environment.getProperty("age"));
+//        System.out.println(environment.getProperty("height"));
+//        System.out.println(environment.getProperty("liming.weight"));
+//        System.out.println("> args");
+//        System.out.println(Arrays.toString(args));
+//        System.out.println(environment.getProperty("me.weight"));
+//        System.out.println(environment.getProperty("she.weight"));
+//        System.out.println(environment.getProperty("liming.weight"));
+//
+//        System.out.println();
+//
+//        System.out.println(">>> system");
+//        System.out.println(System.getProperty("class"));
+//        System.out.println("> system properties");
+//        System.out.println(System.getProperty("name"));
+//        System.out.println(System.getProperty("age"));
+//        System.out.println(System.getProperty("height"));
+//        System.out.println(System.getProperty("liming.weight"));
+//        System.out.println("> args");
+//        System.out.println(Arrays.toString(args));
+//        System.out.println(System.getProperty("me.weight"));
+//        System.out.println(System.getProperty("she.weight"));
+//        System.out.println(System.getProperty("liming.weight"));
+    }
+
+}
