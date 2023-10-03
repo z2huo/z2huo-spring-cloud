41556e01205c8ca9e7e23f7b46a2fcbb40f72157
z2huo <z2huo9994@163.com>
2023-10-03 23:30:39 +0800

    common module

---
+++ z2huo-spring-cloud-common/z2huo-spring-cloud-common-core/src/main/java/cn/z2huo/common/core/constant/ServiceNameConstants.java
@@ -0,0 +1,13 @@
+package cn.z2huo.common.core.constant;
+
+public interface ServiceNameConstants {
+
+    String EUREKA_CLIENT_DEMO = "eureka-client";
+
+    String EUREKA_CLIENT_CONSUMER = "eureka-client-consumer";
+
+    String EUREKA_CLIENT_PROVIDER_1 = "eureka-client-provider-1";
+
+    String EUREKA_CLIENT_PROVIDER_2 = "eureka-client-provider-2";
+
+}
