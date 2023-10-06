1ee483f89719914a3f7032691258d6eedd73711a
z2huo <z2huo9994@163.com>
2023-10-06 22:50:41 +0800

    configuration update

--- z2huo-spring-cloud-alibaba-provider-1/src/main/java/cn/z2huo/springcloud/alibaba/provider1/config/ProjectInfo2Configuration.java
+++ z2huo-spring-cloud-alibaba-provider-1/src/main/java/cn/z2huo/springcloud/alibaba/provider1/config/ProjectInfo2Configuration.java
@@ -1,9 +1,13 @@
 package cn.z2huo.springcloud.alibaba.provider1.config;
 
-import lombok.Data;
+import lombok.Getter;
+import lombok.Setter;
+import lombok.ToString;
 import org.springframework.boot.context.properties.ConfigurationProperties;
 
-@Data
+@Getter
+@Setter
+@ToString
 @ConfigurationProperties(prefix = "project")
 public class ProjectInfo2Configuration {
 
--- z2huo-spring-cloud-alibaba-provider-1/src/main/java/cn/z2huo/springcloud/alibaba/provider1/config/ProjectInfoConfiguration.java
+++ z2huo-spring-cloud-alibaba-provider-1/src/main/java/cn/z2huo/springcloud/alibaba/provider1/config/ProjectInfoConfiguration.java
@@ -1,13 +1,17 @@
 package cn.z2huo.springcloud.alibaba.provider1.config;
 
 import cn.z2huo.springcloud.alibaba.provider1.entity.Project;
-import lombok.Data;
+import lombok.Getter;
+import lombok.Setter;
+import lombok.ToString;
 import org.springframework.boot.context.properties.ConfigurationProperties;
 import org.springframework.cloud.context.config.annotation.RefreshScope;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 
-@Data
+@Getter
+@Setter
+@ToString
 @Configuration
 @ConfigurationProperties(prefix = "project")
 public class ProjectInfoConfiguration {
